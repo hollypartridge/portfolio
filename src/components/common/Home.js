0552c2eba7a2fbbfedd81d75c928a6ab0460d4de
hollypartridge <holpartridge@gmail.com>
2022-03-01 14:45:49 +0000

    videos

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,6 +1,10 @@
 import WebsiteDisplay from './WebsiteDisplay'
 import ProjectInfo from './ProjectInfo'
 import Butterfly from './Butterfly'
+import SpaceInvadersVideo from '../../assets/space-invaders-walkthrough.mp4'
+import BflyVideo from '../../assets/b-fly-walkthrough.mp4'
+import EpopVideo from '../../assets/e-pop-walkthrough.mp4'
+import OnlineCuratedVideo from '../../assets/online-curated-walkthrough.mp4'
 
 function Home() {
   return (
@@ -9,7 +13,7 @@ function Home() {
         <Butterfly emoji='🦋'/>
         <WebsiteDisplay 
           title='online-curated.netlify.app'
-          video='https://vod-progressive.akamaized.net/exp=1646220000~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3635%2F26%2F668177216%2F3075517360.mp4~hmac=763d0733c5a48bd2bd46ab8515052b0ae1ec410c4907f512f63bbfbb19715ac0/vimeo-prod-skyfire-std-us/01/3635/26/668177216/3075517360.mp4?filename=540p.mp4'
+          video={OnlineCuratedVideo}
         />
         <ProjectInfo 
           link='/projects/online-curated' 
@@ -23,7 +27,7 @@ function Home() {
         />
         <WebsiteDisplay 
           title='e-pop.netlify.app'
-          video='https://player.vimeo.com/external/664664332.sd.mp4?s=1588885464511681ae6e544e86b1f1240d28f975&profile_id=165&oauth_token_id=1150485861'
+          video={EpopVideo}
         />
         <Butterfly emoji='🦋' />
       </section>
@@ -31,7 +35,7 @@ function Home() {
         <Butterfly emoji='🦋' />
         <WebsiteDisplay 
           title='tarot-reading-react.netlify.app'
-          video='https://vod-progressive.akamaized.net/exp=1646220000~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3635%2F26%2F668177216%2F3075517360.mp4~hmac=763d0733c5a48bd2bd46ab8515052b0ae1ec410c4907f512f63bbfbb19715ac0/vimeo-prod-skyfire-std-us/01/3635/26/668177216/3075517360.mp4?filename=540p.mp4'
+          video={BflyVideo}
         />
         <ProjectInfo 
           link='/projects/b-fly' 
@@ -45,7 +49,7 @@ function Home() {
         />
         <WebsiteDisplay 
           title='hollypartridge.github.io/Space-Invaders-GA-Project-1'
-          video='https://player.vimeo.com/external/664664332.sd.mp4?s=1588885464511681ae6e544e86b1f1240d28f975&profile_id=165&oauth_token_id=1150485861'
+          video={SpaceInvadersVideo}
         />
         <Butterfly emoji='🦋' />
       </section>
--- src/components/common/ProjectShow.js
+++ src/components/common/ProjectShow.js
@@ -1,5 +1,9 @@
 import { useParams } from 'react-router-dom'
 
+import SpaceInvadersVideo from '../../assets/space-invaders-walkthrough.mp4'
+import BflyVideo from '../../assets/b-fly-walkthrough.mp4'
+import EpopVideo from '../../assets/e-pop-walkthrough.mp4'
+import OnlineCuratedVideo from '../../assets/online-curated-walkthrough.mp4'
 import WebsiteDisplay from './WebsiteDisplay'
 
 function ProjectShow() {
@@ -9,7 +13,7 @@ function ProjectShow() {
       hyperlink: 'https://online-curated.netlify.app/',
       website: 'online-curated.netlify.app',
       description: 'A full-stack app built in 1 week using Python, Django, PostgreSQL, React and SCSS. My app, Online Curated, is a womenswear shop. Its features included a shopping bag, wishlist and a wardrobe where users could create outfits from the products, using drag and drop functionality. Building a full stack app on my own helped me develop my knowledge of the back-end, using Python, Django and PostgreSQL for the first time, while pushing myself further on the front-end to creating an app with more diverse functionality.',
-      video: 'https://vod-progressive.akamaized.net/exp=1646220000~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3635%2F26%2F668177216%2F3075517360.mp4~hmac=763d0733c5a48bd2bd46ab8515052b0ae1ec410c4907f512f63bbfbb19715ac0/vimeo-prod-skyfire-std-us/01/3635/26/668177216/3075517360.mp4?filename=540p.mp4',
+      video: OnlineCuratedVideo,
       github: 'https://github.com/hollypartridge/Online-Curated-GA-Project-4-FE',
       id: 'online-curated',
     },
@@ -18,7 +22,7 @@ function ProjectShow() {
       hyperlink: 'https://e-pop.netlify.app/',
       website: 'e-pop.netlify.app',
       description: 'A full-stack MERN App built using React, Node.js, Express and MongoDB in 1 week. Our app was an online community and platform for creatives and developers to share their work and connect with others. For this project, I focused primarily on the front-end, building the main pages of the site using React. This included conditional rendering of the pages depending on authorisation.',
-      video: 'https://vod-progressive.akamaized.net/exp=1646220000~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3635%2F26%2F668177216%2F3075517360.mp4~hmac=763d0733c5a48bd2bd46ab8515052b0ae1ec410c4907f512f63bbfbb19715ac0/vimeo-prod-skyfire-std-us/01/3635/26/668177216/3075517360.mp4?filename=540p.mp4',
+      video: EpopVideo,
       github: 'https://github.com/hollypartridge/Epop-GA-Project-3-FE',
       id: 'e-pop',
     },
@@ -27,7 +31,7 @@ function ProjectShow() {
       hyperlink: 'https://tarot-reading-react.netlify.app/',
       website: 'tarot-reading-react.netlify.app',
       description: 'A React app built using an external API in 48 hours. Our app provided the user with a Tarot Card reading. This project was crucial in developing my knowledge of React, as well as learning to pair program and problem solving as a team.',
-      video: 'https://vod-progressive.akamaized.net/exp=1646220000~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3635%2F26%2F668177216%2F3075517360.mp4~hmac=763d0733c5a48bd2bd46ab8515052b0ae1ec410c4907f512f63bbfbb19715ac0/vimeo-prod-skyfire-std-us/01/3635/26/668177216/3075517360.mp4?filename=540p.mp4',
+      video: BflyVideo,
       github: 'https://github.com/hollypartridge/B-Fly-GA-Project-2',
       id: 'b-fly',
     },
@@ -36,7 +40,7 @@ function ProjectShow() {
       hyperlink: 'https://hollypartridge.github.io/Space-Invaders-GA-Project-1/',
       website: 'hollypartridge.github.io/Space-Invaders-GA-Project-1',
       description: 'A grid based video-game built using HTML, CSS and JavaScript in 1 week. My game was a classic version of Space Invaders. Building this game helped me cement and further evolve my knowledge of JavaScript, while increasing my confidence in developing on my own.',
-      video: 'https://vod-progressive.akamaized.net/exp=1646220000~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3635%2F26%2F668177216%2F3075517360.mp4~hmac=763d0733c5a48bd2bd46ab8515052b0ae1ec410c4907f512f63bbfbb19715ac0/vimeo-prod-skyfire-std-us/01/3635/26/668177216/3075517360.mp4?filename=540p.mp4',
+      video: SpaceInvadersVideo,
       github: 'https://github.com/hollypartridge/Space-Invaders-GA-Project-1',
       id: 'space-invaders',
     }
